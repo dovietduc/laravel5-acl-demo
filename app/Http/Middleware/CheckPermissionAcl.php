b414b0bfe3b0e2efd340e976121c0bd20b98d802
dovietduc <duc.do@j-job.com.vn>
2018-12-27 18:47:50 +0700

    rollback middle ware

--- app/Http/Middleware/CheckPermissionAcl.php
+++ app/Http/Middleware/CheckPermissionAcl.php
@@ -28,38 +28,18 @@ class CheckPermissionAcl
 //            ->select('roles.*')
 //            ->get()->pluck('id')->toArray();
         $listRoleOfUser = User::find(auth()->id())->roles()->select('roles.id')->pluck('id')->toArray();
-//        dd($listRoleOfUser);
-        // phan xoa
-
-
-
-
-        DB::connection()->enableQueryLog();
-        $listPermissionOfUser = Role::with(array('permissions'=>function($query) use ($listRoleOfUser){
-            $query->whereIn('role_permission.role_id', $listRoleOfUser);
-        }))
-            ->get();
-        $queries = DB::getQueryLog();
-        dd($listPermissionOfUser);
-
-        // phan xoa
-
-
-
-
-        // 2. lay tat ca cac quyen khi user login vao he thong
-//        $listRoleOfUser = DB::table('roles')
-//            ->join('role_permission', 'roles.id', '=', 'role_permission.role_id')
-//            ->join('permissions', 'role_permission.permission_id', '=', 'permissions.id')
-//            ->whereIn('roles.id', $listRoleOfUser)
-//            ->select('permissions.*')
-//            ->get()->pluck('id')->unique();
+//         2. lay tat ca cac quyen khi user login vao he thong
+        $listRoleOfUser = DB::table('roles')
+            ->join('role_permission', 'roles.id', '=', 'role_permission.role_id')
+            ->join('permissions', 'role_permission.permission_id', '=', 'permissions.id')
+            ->whereIn('roles.id', $listRoleOfUser)
+            ->select('permissions.*')
+            ->get()->pluck('id')->unique();
         // Lay tat ca cac quyen khi user login vao he thong
-
         // lay ra ma man hinh tuong ung de check phan quyen
         $checkPermission = Permission::where('name', $permission)->value('id');
         // kiem tra user dc phep vao man hinh nay khong?
-        if ( $listPermissionOfUser->contains($checkPermission) ) {
+        if ( $listRoleOfUser->contains($checkPermission) ) {
             return $next($request);
         }
         return abort(401);
